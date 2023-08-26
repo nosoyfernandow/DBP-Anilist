34492e6aa28e7a078d952a7051a1e8f4acf63ea4
nosoyfernandow <112716081+nosoyfernandow@users.noreply.github.com>
2023-08-25 19:55:46 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
-# Datos de ejemplo para la lista de animes
-animes = []
+# Cargar datos desde el archivo anime.json
+with open('anime.json') as f:
+    data = json.load(f)
+animes = data['anime']
 
 @app.route('/')
 def index():
-    return '¡Hola, mundo! Esta es mi primera API con Flask.'
+    return data
 
 @app.route('/anime', methods=['GET'])
 def get_animes():
@@ -15,26 +18,51 @@ def get_animes():
 
 @app.route('/anime/<int:id>', methods=['GET'])
 def get_anime(id):
-    return jsonify({"message": "Obtener detalles del anime con ID " + str(id)})
-
-@app.route('/anime/<int:id>', methods=['DELETE'])
-def delete_anime(id):
-    return jsonify({"message": "Eliminar anime con ID " + str(id)})
+    anime = next((anime for anime in animes if anime['id'] == id), None)
+    if anime:
+        return jsonify(anime)
+    return jsonify({"Anime no encontrado"}), 404
 
 @app.route('/anime', methods=['POST'])
 def create_anime():
-    new_anime = request.get_json()
-    return jsonify(new_anime)
+    new_anime = request.json
+    new_anime['id'] = len(animes) + 1
+    animes.append(new_anime)
+    with open('anime.json', 'w') as f:
+        json.dump(data, f, indent=4)
+    return jsonify(new_anime), 201
 
 @app.route('/anime/<int:id>', methods=['PUT'])
 def update_anime(id):
-    updated_anime = request.get_json()
-    return jsonify(updated_anime)
+    anime = next((anime for anime in animes if anime['id'] == id), None)
+    if anime:
+        updated_anime = request.json
+        anime.update(updated_anime)
+        with open('anime.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        return jsonify(anime)
+    return jsonify({"Anime no encontrado"}), 404
 
 @app.route('/anime/<int:id>', methods=['PATCH'])
-def partial_update_anime(id):
-    updated_fields = request.get_json()
-    return jsonify(updated_fields)
+def patch_anime(id):
+    anime = next((anime for anime in animes if anime['id'] == id), None)
+    if anime:
+        updates = request.json
+        anime.update(updates)
+        with open('anime.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        return jsonify(anime)
+    return jsonify({"Anime no encontrado"}), 404
+
+@app.route('/anime/<int:id>', methods=['DELETE'])
+def delete_anime(id):
+    anime = next((anime for anime in animes if anime['id'] == id), None)
+    if anime:
+        animes.remove(anime)
+        with open('anime.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        return jsonify({"Anime eliminado"})
+    return jsonify({"Anime no encontrado"}), 404
 
 if __name__ == '__main__':
     app.run()
